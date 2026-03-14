af319bbf4ec94ea7ec7242d385e4061354afe45e
Henry Herbert Percy Carpmael <152444618+HenryCarpmael@users.noreply.github.com>
2026-03-14 23:24:22 +0000

    Fixed Archies dumbness

--- src/Drafting/drafting_control.gd
+++ src/Drafting/drafting_control.gd
@@ -7,7 +7,7 @@ var selectedButtonIndex = -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for x in range(20) :
-		var cardDatabaseSize = 100;
+		var cardDatabaseSize = Cards.cardList.size();
 		#This shouold be card database size
 		
 		var cardID = randi_range(0, cardDatabaseSize - 1)
@@ -16,7 +16,9 @@ func _ready() -> void:
 		#newCardButton.icon = the picture for the card
 		#Make the card selected when pressed by updating the selected button index using the pressed signal from this utton also ideally some visual indicator? (depressed etc)
 		buttons.append(newCardButton)
-
+	#sort buttons alphebetically ascending
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- src/globals/cards.gd
+++ src/globals/cards.gd
@@ -22,7 +22,7 @@ enum CARDS {
 # dictionay of CARDS to CardStat objects
 var cardList = {
 	CARDS.JUMP : CardStats.new(CARDS.JUMP, 2, "Ignore all units when moving this turn, must end in a empty space", "Jump", "res://cards/Jump.png", Globals.TARGETS.FRIENDLY_PIECE, 1),
-	CARDS.ASSASSINATE : CardStats.new(CARDS.ASSASSINATE, -1, "If one of your units could move and kill this piece this turn, instead kill it instantly. X is piece's value", "Assassinate", "res://cards/AssassinateFinal.png", Globals.TARGETS.FRIENDLY_PIECES, 1),
+	CARDS.ASSASSINATE : CardStats.new(CARDS.ASSASSINATE, -1, "If one of your units could move and kill this piece this turn, instead kill it instantly. X is piece's value", "Assassinate", "res://cards/AssassinateFinal.png", Globals.TARGETS.FRIENDLY_PIECE, 1),
 	CARDS.ELECTROCUTE : CardStats.new(CARDS.ELECTROCUTE, 3, "Choose target tile, at the beginning of your next turn, if a unit is on this tile it is destroyed. This includes your own pieces", "Electrocute", "res://cards/electrocuteFinal.png", Globals.TARGETS.SQUARE, 1),
 	CARDS.FREEZE : CardStats.new(CARDS.FREEZE, 3, "Choose target enemy piece, until your next turn that piece cannot be moved", "Freeze", "res://cards/Freeze.png", Globals.TARGETS.ENEMY_PIECE, 1),
 	CARDS.HARDCOREPAWN : CardStats.new(CARDS.HARDCOREPAWN, 2, "Target pawn you control can move and take as if it were a king this turn", "Hardcore Pawn", "res://cards/hardcorepawn.png", Globals.TARGETS.FRIENDLY_PIECE, 1),
@@ -33,7 +33,7 @@ var cardList = {
 	CARDS.SACRIFICE : CardStats.new(CARDS.SACRIFICE, 0, "Sacrifice one of your pieces and gain X mana where X is the piece's mana value", "Sacrifice", "res://cards/Sacrifice.png", Globals.TARGET.FRIENDLY_PIECE, 1),
 	CARDS.RITUAL : CardStats.new(CARDS.RITUAL, 0, "End your turn instanly, not being able to play any more cards or make a chess move. However, on the beginning of your next turn gain however much energy you had when you play this card", "Ritual", "res://cards/ritualFinal.png", Globals.TARGETS.NONE, 0),
 	CARDS.SHIELDWALL : CardStats.new(CARDS.SHIELDWALL, 3, "Move up to three unique pawns this turn, if you play this you cannot move any other pieces this turn.", "Shield Wall", "res://cards/shieldwall.png", Globals.TARGETS.NONE, 0),
-	CARDS.SPEAR : CardStats.new(CARDS.SPEAR, 1, "Pawns can also take directly in front of them this turn", "Spear", "res://cards/spear.png", Globals.TARGETS.FRIENDLY_UNIT, 1),
+	CARDS.SPEAR : CardStats.new(CARDS.SPEAR, 1, "Pawns can also take directly in front of them this turn", "Spear", "res://cards/spear.png", Globals.TARGETS.PIECE, 1),
 	CARDS.SWAP : CardStats.new(CARDS.SWAP, 5, "Switch any two pieces, however when you play this you cannot play any more cards this turn or make a chess move (End your turn instantly)", "Swap", "res://cards/swap.png", Globals.TARGETS.PIECE, 2),
-	CARDS.TRIP : CardStats.new(CARDS.TRIP, 2, "Target friendly piece can move into one adjacent empty diagonal tile", "Trip", "res://cards/tripFinal.png", Globals.TARGETS.FRIENDLY_UNIT, 1)
+	CARDS.TRIP : CardStats.new(CARDS.TRIP, 2, "Target friendly piece can move into one adjacent empty diagonal tile", "Trip", "res://cards/tripFinal.png", Globals.TARGETS.FRIENDLY_PIECE, 1)
 }
